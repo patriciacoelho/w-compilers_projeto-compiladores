598cef7e78dc3aee891ef542ca343b63189b681e
Uendel <Uendel>
2018-11-23 19:48:02 -0300

    Inicial

--- src/compilador/Scanner.java
+++ src/compilador/Scanner.java
@@ -2,6 +2,7 @@ package compilador;
 public class Scanner {
     private char currentChar; //TO DO : pegar primeiro caractere do txt
     
+    private int linha, col;
     private byte currentKind;
     private StringBuffer currentValue;
     
@@ -32,19 +33,130 @@ public class Scanner {
     }
     
     private byte scanToken(){
-        return 0;
-        //TO DO
+        if(isLetter(currentChar)){
+            takeIt();
+             while(isLetter(currentChar) || isDigit(currentChar)){ //<letra>(<letra> | <digito>)* 
+                takeIt();
+            }
+            return Token.ID;
+        }
+        if(isDigit(currentChar)){ //<digito><digito>*
+            takeIt();
+            while(isDigit(currentChar)){ 
+                takeIt();
+            }
+            return Token.INT_LIT;
+        }
+        if(currentChar == '+'){
+            takeIt();
+            return Token.SOMA;
+        }
+        if(currentChar == '-'){
+            takeIt();
+            return Token.SUB;
+        }
+        if(currentChar == '*'){
+            takeIt();
+            return Token.MULT;
+        }
+        if(currentChar == '/'){
+            takeIt();
+            return Token.DIV;
+        }
+        if(currentChar == '>'){
+            takeIt();
+            if(currentChar == '='){
+                takeIt();
+                return Token.MAIOR_IGUAL;
+            } else{
+                return Token.MAIORQ;
+            }
+        }
+        if(currentChar == '<'){ // leitura de < <= <>
+            takeIt();
+            if(currentChar == '='){
+                takeIt();
+                return Token.MENOR_IGUAL;
+            } else if(currentChar == '>'){
+                    takeIt();
+                    return Token.DIF;
+            } else {
+                return Token.MENORQ;
+             
+            }        
+        }
+        if(currentChar == '['){
+            takeIt();
+            return Token.LBRACKET; //pode refatorar
+        }
+        if(currentChar == ']'){
+            takeIt();
+            return Token.RBRACKET;
+        }
+        if(currentChar == ';'){
+            takeIt();
+            return Token.PONTO_VG;
+        }
+        if(currentChar == ':'){
+            takeIt();
+            if(currentChar == '='){
+                takeIt();
+                return Token.ATRIBUICAO;
+            } else{
+                return Token.DOIS_PONTOS;
+            }
+        }
+        if(currentChar == '('){
+            takeIt();
+            return Token.LPAREN;
+        }
+        if(currentChar == ')'){
+            takeIt();
+            return Token.RPAREN;
+        }
+        if(currentChar == '.'){
+            takeIt();
+            if(currentChar == '.'){
+                takeIt();
+                return Token.PONTO_PONTO;
+            } else {
+                return Token.PONTO;
+            }
+        }
+        if(currentChar == ','){
+            takeIt();
+            return Token.VIRG;
+        }
+        //TO DO : LER O EOF e o float-lit
+            
+       
+        
+        //TO DO : return erro
     }
     
-    private void scanSeparator(){
-        //TO DO
+    private void scanSeparator(){ //Revisar simbolos
+        switch(currentChar){
+            case '!':{ //marcação de linha de comentário assim como esse //
+                takeIt();
+                while(isGraphic(currentChar)){
+                    takeIt();
+                }
+                take('\n');
+            }
+            break;
+            case ' ':
+            case '\n':
+                takeIt();
+            break;
+        }
     }
+    
     public Token scan(){
-        while(currentChar == '4'){
-            this.scanSeparator();
+        while(currentChar == '!' || currentChar == ' ' || currentChar == '\n'){
+            scanSeparator();
         }
         currentValue = new StringBuffer("");
-        currentKind = this.scanToken();
-        return new Token(currentKind, currentValue.toString()); //TO DO : add linha e coluna
+        currentKind = scanToken();
+        return new Token(currentKind, currentValue.toString(), linha, col); //TO DO : add linha e coluna
     }
 }
--- src/compilador/Token.java
+++ src/compilador/Token.java
@@ -2,15 +2,40 @@ package compilador;
 class Token {
     
     public byte kind;
-    public String spelling;
+    public String value;
+    public int line;
+    public int col;
     
-    public Token(byte kind, String spelling){
+    public Token(byte kind, String value, int line, int col){
         this.kind = kind;
-        this.spelling = spelling;
+        this.value = value;
+        this.line = line;
+        this.col = col;
         
-        if(kind == 4){
-            
+        if(kind == ID){
+            for(byte k = BEGIN;k <= BOOLEAN; k++){
+                if(value.equals(spellings[k])){
+                    this.kind = k;
+                    break;
+                }
+            }
         }
         
     }
+    
+    public final static byte ID = 0, INT_LIT = 1, SOMA = 2, SUB = 3, MULT = 4,
+            DIV = 5, MAIORQ = 6, MAIOR_IGUAL = 7, MENOR_IGUAL = 8, DIF = 9,
+            MENORQ = 10, LBRACKET = 11, RBRACKET = 12, PONTO_VG = 13,
+            ATRIBUICAO = 14, DOIS_PONTOS = 15, LPAREN = 16, RPAREN = 17, 
+            PONTO_PONTO = 18, PONTO = 19, VIRG = 20, BEGIN  = 21, END = 22, 
+            IF = 23, THEN = 24, ELSE = 25, VAR = 26, WHILE = 27, DO = 28,
+            OR = 29, AND = 30, PROGRAM = 31, ARRAY = 32, OF = 33, INTEGER = 34,
+            REAL = 35, BOOLEAN = 36, EOF = 37;
+    
+    private final static String[] spellings = {
+        "<id>", "<int_lit>", "+", "-", "*", "/", ">", ">=", "<=", "<>", ">",
+        "[", "]", ";", ":=", ":", "(", ")", "..",".", ",", "begin", "end", "if",
+        "then", "else", "var","while", "do", "or", "and", "program", "array",
+        "of", "integer", "real", "boolean", "<EOF>"
+    };
 }
