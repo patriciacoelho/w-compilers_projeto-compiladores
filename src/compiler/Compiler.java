89a4404f005a01bc81eacf198cb5312ec48061cf
uesleymelo <uesley.eumesmo@gmail.com>
2019-03-25 01:24:22 -0300

    Obriga o uso de --file para inserir caminho do arquivo e para o compilador se algum comando de debug foi passado com exceção de --all

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -17,13 +17,13 @@ public class Compiler {
     public static boolean _PARSER_DEBUG_ = false;
     public static boolean _CHECKER_DEBUG_ = false;
     public static boolean _CODER_DEBUG_ =false;
-    public static String path;
+    public static boolean _ALL_ = false;
+    public static String path = null;
     
     public static void main(String args[]) throws Exception{
-        path = (args.length == 0) ? "src/programa.pas" : args[0];
         Programa program;
-        
-        for (String arg : args){
+        for (int i = 0; i < args.length ; i++){
+            String arg = args[i];
             switch(arg) {
                 case "--lexical": _LEXICAL_DEBUG_ = true; break;
                 case "--parser" : _PARSER_DEBUG_  = true; break;
@@ -33,7 +33,18 @@ public class Compiler {
                                   _PARSER_DEBUG_  = true;
                                   _CHECKER_DEBUG_ = true;
                                   _CODER_DEBUG_   = true;
+                                  _ALL_           = true; break;
+                case "--file"   : 
+                                  if (i+1 >= args.length) {
+                                      System.out.println("Erro: o caminho do arquivo deve ser informado após --file");
+                                      System.exit(1);
+                                  }   
+                                  path = args[i+1];
+                                  break;
             }
+        }
+        if (path == null){
+            path = "src/programa.pas";
         }
         if (_LEXICAL_DEBUG_) {
         	System.out.println("============ Lexical Debugger ============\n");
@@ -46,20 +57,29 @@ public class Compiler {
 	            tk.print();
 	        } while(tk.kind != Token.EOF);
                 System.out.println("\n\n");
+                if (!_ALL_) {
+                    System.exit(0);
+                }
         }
         Parser parser = new Parser();
-        Printer printer = new Printer();
-        Checker checker = new Checker();
-        Coder coder = new Coder();
         program = parser.parse(path);
         if (_PARSER_DEBUG_) {
+            Printer printer = new Printer();
             printer.print(program);
             System.out.println("\n\n");
+            if (!_ALL_) {
+                System.exit(0);
+            }
         }
+        Checker checker = new Checker();
         checker.check(program);
         if (_CHECKER_DEBUG_) {
             checker.table.print();
+            if (!_ALL_) {
+                System.exit(0);
+            }
         }
+        Coder coder = new Coder();
         coder.code(program);
     }
 }
